5723a017581a86c292f9aed47d2ee5ce4e6588cf
Mesinger <uintptr96@gmail.com>
2018-12-29 21:07:00 +0100

    sp for listing products enhanced

--- sql/baumgartinger/sp_list_insurance.sql
+++ sql/baumgartinger/sp_list_insurance.sql
@@ -14,9 +14,14 @@ AS
 		IF (SELECT COUNT(*) FROM PRODUKT_KATEGORIE WHERE PRODUKT_KATEGORIE.ID = @category_id) != 1
 			THROW 60000, 'invalid category id', 1
 
-		SELECT NAME, BESCHREIBUNG
-		FROM PRODUKTE
-		WHERE FK_ID_PRODUKT_KATEGORIE = @category_id
+		SELECT	prods.NAME as category, BESCHREIBUNG as description,
+				PREIS as price, LAUFZEIT as duration, anb.NAME as vendor_name, ADRESSE as address
+				FROM PRODUKTE AS prods
+					FULL OUTER JOIN PRODUKTE_ANBIETER AS prod_anb
+					ON prods.ID = prod_anb.FK_ID_PRODUKTE
+					FULL OUTER JOIN ANBIETER AS anb
+					ON anb.ID = prod_anb.FK_ID_ANBIETER
+				WHERE prods.FK_ID_PRODUKT_KATEGORIE = @category_id
 
 		COMMIT TRANSACTION
 
