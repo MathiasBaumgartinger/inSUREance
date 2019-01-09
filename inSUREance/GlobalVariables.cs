632c0a0523f680963a449b0e96992733a24b6895
MathiasBaumgartinger <33001106+MathiasBaumgartinger@users.noreply.github.com>
2019-01-09 11:04:07 +0100

    Rating now works properly

--- inSUREance/GlobalVariables.cs
+++ inSUREance/GlobalVariables.cs
@@ -15,11 +15,13 @@ namespace inSUREance
             private static string name;
             private static DateTime birthday;
             private static string address;
+            private static int id;
 
             public static string Name { get { return name; } set { name = value; } }
             public static DateTime Birthday { get { return birthday; } set { birthday = value; } }
             public static string Address { get { return address; } set { address = value; } }
-        
+            public static int Id { get { return id; } set { id = value; } }
+
             // TODO: make database stuff
         }
 
---
+++ inSUREance/db/ProviderStatement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inSUREance.db
+{
+    class GetProviderIDStatement : IPreparedStatement
+    {
+        private readonly string name;
+
+        public GetProviderIDStatement(string name)
+        {
+            this.name = name;
+        }
+
+        public override void Prepare(SqlConnection connection)
+        {
+            command.Connection = connection;
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.CommandText = "select_providerId_by_name";
+
+            SqlParameter providerNameParam = new SqlParameter("@provider", SqlDbType.VarChar, 30);
+            providerNameParam.Value = name;
+
+            command.Parameters.Add(providerNameParam);
+            command.Prepare();
+        }
+    }
+}
